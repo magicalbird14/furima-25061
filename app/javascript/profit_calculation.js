a02e0e13a09480714481f8dc3eae1839ffcc83e3
magicalbird14 <favorite091107@gmail.com>
2022-11-13 22:27:15 +0900

    Fire only on new page

--- app/javascript/profit_calculation.js
+++ app/javascript/profit_calculation.js
@@ -1,12 +1,16 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    var commission = Math.floor(inputValue/10)
-    addTaxDom.innerHTML = commission
-    const addProfitDom = document.getElementById("profit");
-    var profit = Math.floor(inputValue - commission)
-    addProfitDom.innerHTML = profit
+if(location.pathname.match(/\/items\/new/)){
+
+  window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      var commission = Math.floor(inputValue/10)
+      addTaxDom.innerHTML = commission
+      const addProfitDom = document.getElementById("profit");
+      var profit = Math.floor(inputValue - commission)
+      addProfitDom.innerHTML = profit
+    })
   })
-})
+  
+}
